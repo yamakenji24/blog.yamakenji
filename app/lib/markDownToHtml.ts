a4a9adb8831d2833368e000e525f977817d0eef2
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2021-12-08 21:02:37 +0900

    chore

--- app/lib/markDownToHtml.ts
+++ app/lib/markDownToHtml.ts
@@ -1,8 +1,10 @@
 export async function markDownToHtml(content: string): Promise<string> {
-  const { remark } = await import('remark');
-  const html = await import('remark-html');
-  const gfm = await import('remark-gfm');
+  const [remark, html, gfm] = await Promise.all([
+    import('remark').then((mod) => mod.remark),
+    import('remark-html').then((mod) => mod.default),
+    import('remark-gfm').then((mod) => mod.default),
+  ]);
 
-  const result = await remark().use(gfm.default).use(html.default).process(content);
+  const result = await remark().use(html).use(gfm).process(content);
   return result.toString();
 }
