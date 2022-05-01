7583032e9c57836c81079226a4fbf7b6f5f5c8ea
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2022-05-02 00:56:50 +0900

    feat: configs for i18n

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,14 +1,42 @@
 import { renderToString } from 'react-dom/server';
 import { RemixServer } from 'remix';
 import type { EntryContext } from 'remix';
+import { createInstance } from 'i18next';
+import Backend from 'i18next-fs-backend';
+import { resolve } from 'node:path';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+import { i18n } from './i18n.server';
 
-export default function handleRequest(
+export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const markup = renderToString(<RemixServer context={remixContext} url={request.url} />);
+  const instance = createInstance();
+  const lng = await i18n.getLocale(request);
+  const ns = i18n.getRouteNamespaces(remixContext);
+
+  await instance
+    .use(initReactI18next)
+    .use(Backend)
+    .init({
+      supportedLngs: ['ja', 'en'],
+      defaultNS: 'common',
+      fallbackLng: 'ja',
+      react: { useSuspense: false },
+      ns,
+      lng,
+      backend: {
+        loadPath: resolve(__dirname, '../locales/{{lng}}/{{ns}}.json'),
+      },
+    });
+
+  const markup = renderToString(
+    <I18nextProvider i18n={instance}>
+      <RemixServer context={remixContext} url={request.url} />
+    </I18nextProvider>,
+  );
 
   responseHeaders.set('Content-Type', 'text/html');
 
