6543ddf43be7eab594912165c7eadeb50c2f7242
yamakenji24 <45092341+yamakenji24@users.noreply.github.com>
2022-06-12 23:03:23 +0900

    Refactor/footer (#38)
    * Story for Footer
    
    * Header dir

--- app/components/common/Header/Header.test.tsx
+++ app/components/common/Header/Header.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import '@testing-library/jest-dom';
-import { Header } from '../Header';
+import { Header } from '.';
 
 // WIP: Router周辺のテストがよくわからない
 describe('render Header', () => {
