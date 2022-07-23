549385bf17daf4b4c4e35dd420b40d4b578fe84e
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2022-07-23 23:35:02 +0900

    fixed accessibility

--- app/components/common/Footer/Footer.tsx
+++ app/components/common/Footer/Footer.tsx
@@ -7,10 +7,10 @@ function Footer() {
   const year = React.useMemo(() => new Date().getFullYear(), []);
 
   return (
-    <footer className="mx-auto bg-green-600 text-white p-2">
+    <footer className="mx-auto bg-green-700 text-white p-2">
       <div className="flex justify-center space-x-4">
         <p>&copy; {year} yamakenji24, All rights reserved.</p>
-        <a href={GITHUB_URL} target="_blank" rel="noopener noreferrer">
+        <a aria-label="My Github Url" href={GITHUB_URL} target="_blank" rel="noopener noreferrer">
           <FaGithub fontSize="24px" />
         </a>
       </div>
