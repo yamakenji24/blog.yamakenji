b1851f7be085e69ef0ea8b9843916c8c3a10aee9
yamakenji24 <yamakenji24@gmail.com>
2023-05-07 01:32:54 +0900

    code smellの対応

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction, LoaderArgs } from '@remix-run/node';
+import type { LoaderArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 
