aa5085b3f99751bf6c13909efa67e6b45b2eb67a
yamakenji24 <yamakenji24@gmail.com>
2023-12-26 15:51:34 +0900

    migration settings to v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,14 @@
 module.exports = {
   "typescript" : { reactDocgen: false },
-  "stories": [
+
+  stories: [
     "../app/components/**/*.stories.@(js|jsx|ts|tsx)",
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
     {
       name: '@storybook/addon-postcss',
       options: {
@@ -19,14 +18,15 @@ module.exports = {
       },
     },
   ],
-  "framework": "@storybook/react",
-  core: {
-    builder: {
-      name: 'webpack5',
-      options: {
+
+  framework: {
+    name: "@storybook/react-webpack5",
+
+    options: {
+      builder: {
         lazyCompilation: true,
-        fsCache: true,
-      },
+        fsCache: true
+      }
     }
   }
 }
